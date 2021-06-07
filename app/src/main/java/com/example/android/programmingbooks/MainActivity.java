d7b5714d288450f8626cb190de2352708ab5cd4c
Norheem <nohimsulaiman@gmail.com>
2021-06-07 11:47:32 +0100

    Update on the Programming App project

--- app/src/main/java/com/example/android/programmingbooks/MainActivity.java
+++ app/src/main/java/com/example/android/programmingbooks/MainActivity.java
@@ -1,14 +1,131 @@
 package com.example.android.programmingbooks;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+    String mTitle[] = {"Java", "Python", "C++", "HTML", "JavaScript", "Ruby"};
+    String[] mDescription = {"Introduction to Java Programming", "Learn Python in 30 days", "Learn C++ in 7 days", "Learn HTML in 5 days", "Introduction to JavaScript", "Introduction to Ruby Language"};
+    int mImage[] = {R.drawable.java, R.drawable.python, R.drawable.cpp, R.drawable.html, R.drawable.js, R.drawable.ruby};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+
+        //Now create an adapter class
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, mDescription, mImage);
+        listView.setAdapter(adapter);
+
+        // now set item click on list view
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Introduction to Java Programming", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Learn Python in 30 days", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Learn C++ in 7 days", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Learn HTML in 5 days", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Introduction to JavaScript", Toast.LENGTH_SHORT).show();
+                }
+                if (position == 0) {
+                    Toast.makeText(MainActivity.this, "Introduction to Ruby Language", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // so item click  is done now check list view
     }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String rTitle[];
+        String  rDescription[];
+        int rImgs[];
+
+        MyAdapter (Context c, String title[], String description[], int imgs[]) {
+            super(c, R.layout.row, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rDescription = description;
+            this.rImgs = imgs;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View row = layoutInflater.inflate(R.layout.row, parent, false);
+            ImageView images = row.findViewById(R.id.image);
+            TextView myTitle = row.findViewById(R.id.textView1);
+            TextView mDescription = row.findViewById(R.id.textView2);
+
+            // now set our resources on view
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+            mDescription.setText(rDescription[position]);
+
+            return row;
+    }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        // Retrieve the SearchView and plug it into SearchManager
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.search_icon));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+            switch (item.getItemId())
+            {
+                case R.id.search_icon:
+                    return true;
+                case R.id.settings:
+                    return true;
+                case R.id.refresh:
+                    return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+    }
+
 }
